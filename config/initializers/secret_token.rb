e4b0a069f5d793a2a0bef3b82665e011d85db945
Paulo Choi <mail@paulochoi.com>
2013-05-05 20:26:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Everyly::Application.config.secret_token = '59652fa450d4d5678aef8c2bad7fb87c60849e119803a13570bef7185d50ecf4dd5878caf8af998cd673d360b67be80d5cb4f9650cb210f5e1700bca0808fae1'
